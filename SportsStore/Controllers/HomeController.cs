d618ba3e1bbb29160fdf17d6c67f007b1e9ffcb0
Junior <Josejuarez.Junior@smartm.com>
2023-05-30 08:43:11 -0300

    Adding View Model and pagination for Products

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,17 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers 
 {
 	public class HomeController: Controller 
 	{
 		private IStoreRepository repository;
+		public int PageSize = 4;
 		
 		public HomeController(IStoreRepository repo)
 		{
 			repository = repo;
 		}
 		
-		public IActionResult Index() => View(repository.Products);
+		public IActionResult Index(int productPage = 1) 
+			=> View(new ProductsListViewModel 
+			{
+				Products = repository.Products
+					.OrderBy(p => p.ProductID)
+					.Skip((productPage - 1) * PageSize)
+					.Take(PageSize),
+				PagingInfo =  new PagingInfo
+				{
+					CurrentPage = productPage,
+					ItemsPerPage = PageSize,
+					TotalItems = repository.Products.Count()
+				}
+			});
 	}
 }
